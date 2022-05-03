966c1cb1dc914cd158ae3e2a02bb5f46b15f886d
Miqueias <miqueias.sousa.ti@gmail.com>
2022-05-03 05:33:43 -0300

    Feed Editor (2/2)

---
+++ src/controllers/PostController.php
@@ -0,0 +1,34 @@
+<?php
+namespace src\controllers;
+
+use \core\Controller;
+use src\handlers\LoginHandler;
+use src\handlers\PostHandler;
+
+class PostController extends Controller {
+
+    // Armazena usuario que esta logado
+    private $loggedUser;
+
+    public function __construct()
+    {
+        $this->loggedUser = LoginHandler::checkLogin();
+        if($this->loggedUser === false) {
+            $this->redirect('/login');
+        }
+    }
+
+    public function new() {
+        $body = filter_input(INPUT_POST, 'body');
+
+        if($body) {
+            PostHandler::addPost(
+                $this->loggedUser->id,
+                'text',
+                $body
+            );
+        }
+
+        $this->redirect('/');
+    }
+} 

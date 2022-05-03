9096ddf2178ae11d14751873f5119e296f0e3a40
Miqueias <miqueias.sousa.ti@gmail.com>
2022-05-03 04:37:07 -0300

    Teste

--- src/views/partials/feed-editor.php
+++ src/views/partials/feed-editor.php
@@ -2,9 +2,9 @@
                         <div class="box-body">
                             <div class="feed-new-editor m-10 row">
                                 <div class="feed-new-avatar">
-                                    <img src="media/avatars/avatar.jpg" />
+                                    <img src="<?=$base;?>/media/avatars/<?=$user->avatar;?>" />
                                 </div>
-                                <div class="feed-new-input-placeholder">O que você está pensando, Bonieky?</div>
+                                <div class="feed-new-input-placeholder">O que você está pensando, <?=$user->name;?></div>
                                 <div class="feed-new-input" contenteditable="true"></div>
                                 <div class="feed-new-send">
                                     <img src="assets/images/send.png" />
